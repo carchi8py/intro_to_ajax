1cfb366179c19c3a4a27ae1cb8d24a3135e9b125
Chris Archibald <carchi8py@gmail.com>
2015-08-10 21:40:10 -0700

    Adding New York Times data to our page.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ function loadData() {
     var stretviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=1200x800&location=' + street + ',' + city + '"';
     console.log(stretviewURL);
 
+    var nytKey = 'f04dc6617122670611c89974ff297d6b:17:72661996'
+    var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest&api-key=' + nytKey;
+
     $greeting.text('So, you want to live at ' + street + ', ' + city + '?');
 
     // clear out old data before new request
@@ -21,6 +24,19 @@ function loadData() {
 
     // load streetview
     $body.append('<img class="bgimg" src="' + stretviewURL + '>');
+
+    // Getting NYTimes data
+
+    $.getJSON(nytURL, function(data) {
+        $nytHeaderElem.text('New York Time Articles About ' + city);
+
+        articles = data.response.docs;
+        console.log(nytURL)
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + '<a href="'+article.web_url+'">'+article.headline.main+'</a><p>' + article.snippet + '</p></li>');
+        };
+    });
 
 
     // YOUR CODE GOES HERE!
