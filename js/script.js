58b8bfbb7198b45a45c792340a21483eadf2067c
Chris Archibald <carchi8py@gmail.com>
2015-08-10 21:14:23 -0700

    added google street view image to the background.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 
 function loadData() {
 
+    // Variables
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var stretviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=1200x800&location=' + street + ',' + city + '"';
+    console.log(stretviewURL)
+
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
+    $body.append('<img class="bgimg" src="' + stretviewURL + '>');
+
 
     // YOUR CODE GOES HERE!
 
